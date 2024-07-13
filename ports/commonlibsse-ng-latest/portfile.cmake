7588da338189d74268a24b179a8d383963db14f1
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2024-07-13 12:19:33 +0100

    chore: update clib-ng-latest portfile

--- ports/commonlibsse-ng-latest/portfile.cmake
+++ ports/commonlibsse-ng-latest/portfile.cmake
@@ -16,6 +16,16 @@ vcpkg_install_cmake()
 vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
 vcpkg_copy_pdbs()
 
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
+)
+
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
+vcpkg_copy_pdbs()
+
 file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
 file(INSTALL "${SOURCE_PATH}/cmake/CommonLibSSE.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
@@ -24,7 +34,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE")
 
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-        RENAME copyright)
-
+    INSTALL "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright)
