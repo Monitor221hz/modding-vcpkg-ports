6a4c8ce8fb035a76c316c1940cfac9b3760fcdf0
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2024-07-13 14:10:21 +0100

    fix: remove unecessary arg

--- ports/commonlibsse-ng-latest/portfile.cmake
+++ ports/commonlibsse-ng-latest/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
-    OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on -DCMAKE_CXX_STANDARD=23
+    OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
 )
 
 vcpkg_install_cmake()
